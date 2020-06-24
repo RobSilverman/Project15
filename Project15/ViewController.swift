089842bd83d1c33300df9f8c10cdbca7b4031f9b
Rob Silverman <robert.silverman@willowtreeapps.com>
2020-06-24 18:33:20 -0400

    setup

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentAnimation = 0
+    var imageView: UIImageView {
+        let iv = UIImageView(image: UIImage(named: "penguin"))
+        iv.center = CGPoint(x: 512, y: 384)
+        return iv
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(imageView)
     }
 
-
+    @IBAction func tapped(_ sender: Any) {
+        currentAnimation += 1
+        if currentAnimation > 7 { currentAnimation = 0 }
+    }
+    
 }
 
