83900b17adc85e01eee777521c3aa2b35849b493
Rob Silverman <robert.silverman@willowtreeapps.com>
2020-06-25 11:06:07 -0400

    add cycle of animations

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -23,7 +23,45 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
     }
 
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                break
+            case 1:
+                self.imageView.transform = .identity
+                break
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -200, y: -200)
+                break
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                break
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.05
+                self.imageView.backgroundColor = .blue
+                break
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
+            default:
+                break
+            }
+        }) {
+            finished in
+            sender.isHidden = false
+        }
+        
+        
+        
+        
         currentAnimation += 1
         if currentAnimation > 7 { currentAnimation = 0 }
     }
